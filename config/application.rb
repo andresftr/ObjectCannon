2f75d2b81397b3c93564bfb1aef1ba40a0fe9e8f
AndresFTR <afelipe0113@gmail.com>
2019-04-09 15:39:23 -0500

    changes in the factory bot configuration

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,12 @@ module Objectcannon
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.factory_bot.definition_file_paths = ["spec/factories"]
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
